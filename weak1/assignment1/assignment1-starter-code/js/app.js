a0b244de610eb771c63f6a877d25aa1d473d96a5
Studio678 <murzinaa1987@gmail.com>
2017-06-21 14:25:50 +0500

    weak 1 assinment1 complete with bonuses tag w1as1fc

--- weak1/assignment1/assignment1-starter-code/js/app.js
+++ weak1/assignment1/assignment1-starter-code/js/app.js
@@ -1,21 +1,55 @@
 (function () {
 'use strict';
 
-angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+angular.module('LunchCheck', ['ngSanitize'])
+.controller('LunchCheckController', LunchCheckController);
 
-MsgController.$inject = ['$scope'];
-function MsgController($scope) {
-  $scope.name = "Yaakov";
-  $scope.stateOfBeing = "hungry";
+LunchCheckController.$inject = ['$scope','$sce'];
+function LunchCheckController($scope,$sce) {
+  $scope.inputArray = "";
+  $scope.html = "";
+  $scope.message = "We don't consider empty items!!!";
 
-  $scope.sayMessage = function () {
-    return "Yaakov likes to eat healthy snacks at night!";
+  $scope.checkInput = function() {
+  	var splitInputArray = $scope.inputArray.split(',');
+  	var numberOfEmptyStrings = 0;
+  	for (var i = 0; i < splitInputArray.length; i++) {
+  		if(splitInputArray[i] === "" || splitInputArray[i] === " " || splitInputArray[i] === "  " || splitInputArray[i] === "   "){
+  			numberOfEmptyStrings++;
+  		};
+	};
+  	//console.log(numberOfEmptyStrings);
+  	if((splitInputArray.length - numberOfEmptyStrings) > 3){  			
+	  		$scope.html = '<span style="color:green;border:solid 2px green;">Too much!</span>';
+	  		
+	};
+	if((splitInputArray.length - numberOfEmptyStrings) < 4 ){
+		var checkIfAllEmpty = true;
+	  	for (var i = 0; i < splitInputArray.length; i++) {
+	  		if(splitInputArray[i]){
+	  			checkIfAllEmpty = false;
+	  		};
+		};	
+	  	if(checkIfAllEmpty){
+	  		$scope.html = '<span style="color:red;border:1px solid red;">Please enter data first</span>'; 
+	  		
+	  		
+	  	}else{
+	  		$scope.html = '<span style="color:green;border:1px solid green;">Enjoy!</span>';
+	  		
+	  	};
+
+	  	
+	};
+	//console.log(splitInputArray);
+	//$scope.coloredOutput = $interpolate('<font color ="red">{{output}}</font>')($scope);
+	$scope.htmlFunc = function(){
+		return $sce.trustAsHtml($scope.html);	
+	}
+	
   };
 
-  $scope.feedYaakov = function () {
-    $scope.stateOfBeing = "fed";
-  };
+  
 }
 
 })();
